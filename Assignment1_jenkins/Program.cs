a22df39cf7f7cf4074b0abccc0a644245eb1e1b9
gourav237-tiwari <gouravtiwari237@gmail.com>
2022-08-08 16:28:04 +0530

    ini

---
+++ Assignment1_jenkins/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1_jenkins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hiii This is the Assignemnt 1 of jenkins using console");
+        }
+    }
+}
